b3675b67e8105e981515c925520715d5f07c7d66
L-Gaudet <lgaudet@chapman.edu>
2024-04-08 22:13:21 -0700

    adds connected creators, shared layouts

--- app/controllers/creator_controller.rb
+++ app/controllers/creator_controller.rb
@@ -1,6 +1,8 @@
 class CreatorController < ApplicationController 
   before_action :authenticate_creator!
+  layout 'creator'
+
   def index
-  
+    @page = 'dashboard'
   end
 end
--- app/controllers/manage_invites_controller.rb
+++ app/controllers/manage_invites_controller.rb
@@ -1,12 +1,17 @@
 class ManageInvitesController < HomeController
+  layout "creator"
   def index
     @invitations = Invitation.where(creator: current_creator, status: :pending)
+    @page = "manage_invites"
   end
 
   def update
     invite = Invitation.find(params[:invite_id])
     if params[:decision] == "accept"
       invite.update!(status: :accepted)
+      if !current_creator.brand_owners.exists?(invite.brand_owner.id)
+        current_creator.brand_owners << invite.brand_owner
+      end
       redirect_to manage_invites_path, notice: "Invitation accepted"
     else 
       invite.update!(status: :rejected)
